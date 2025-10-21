bed8d8ae984d6c31deb6272b27c7540109781d64
KonstantinNovakovic <novakovickonstantin357@gmail.com>
2025-10-21 08:57:10 +0200

    add working logic for populating number of squares on the fixed size container

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 const container = document.getElementById("container");
+const input = document.getElementById("input");
+const button = document.getElementById("btn");
 
-for (let i = 0; i < 256; i++) {
-  const newElement = document.createElement("div");
-  newElement.classList.add("row");
-  container.append(newElement);
+function doSomething() {
+  container.innerHTML = "";
+
+  let numberOfSquaresPerRow = input.value;
+  console.log(numberOfSquaresPerRow);
+  for (let i = 0; i < numberOfSquaresPerRow * numberOfSquaresPerRow; i++) {
+    newElement = document.createElement("div");
+    newElement.classList.add("row");
+    newElement.style.height =
+      Math.sqrt(160000 / (numberOfSquaresPerRow * numberOfSquaresPerRow)) +
+      "px";
+    newElement.style.width =
+      Math.sqrt(160000 / (numberOfSquaresPerRow * numberOfSquaresPerRow)) +
+      "px";
+
+    container.append(newElement);
+  }
 }
+
+button.addEventListener("click", doSomething);
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault;
+    button.click();
+  }
+});
+
+container.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("row")) {
+    e.target.classList.add("red");
+  }
+});
